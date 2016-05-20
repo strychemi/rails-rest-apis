8b77099da77b4394846da66d247bb2fde1c46de3
Andrew Baik <and.baik@gmail.com>
2016-05-20 05:18:45 -0700

    global var config

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,7 +1,7 @@
 class StatusesController < ApplicationController
   def index
 
-    @statuses = twitter_client.user_timeline("strychemi", count: 5)
+    @statuses = $twitter_client.user_timeline("strychemi", count: 5)
     respond_to do |format|
       format.html
       format.json { render json: @statuses }
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-twitter_client = Twitter::REST::Client.new do |config|
+$twitter_client = Twitter::REST::Client.new do |config|
   config.consumer_key        = ENV["CONSUMER_KEY"]
   config.consumer_secret     = ENV["CONSUMER_SECRET"]
   config.access_token        = ENV["ACCESS_TOKEN"]
