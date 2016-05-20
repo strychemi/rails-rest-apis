59c45a13973a66a961ba972a98c15cdffc3b43db
Andrew Baik <and.baik@gmail.com>
2016-05-20 05:00:40 -0700

    trying out in controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,8 +1,13 @@
 class StatusesController < ApplicationController
   def index
-    twitter = TwitterAPI.new.twitter_client
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
 
-    @statuses = twitter.user_timeline("strychemi", count: 5)
+    @statuses = client.user_timeline("strychemi", count: 5)
     respond_to do |format|
       format.html
       format.json { render json: @statuses }
