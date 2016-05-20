c80d98b6ac1f32af9c2b155cf0cacbd63fa50dfa
Andrew Baik <and.baik@gmail.com>
2016-05-20 04:34:04 -0700

    added twitter gem

---
+++ app/controllers/statuses_controller.rb
@@ -0,0 +1,9 @@
+class StatusesController < ApplicationController
+  def index
+    @statuses = twitter_client.user_timeline("strychemi", count: 5)
+    respond_to do |format|
+      format.html
+      format.json { render json: @statuses }
+    end
+  end
+end
