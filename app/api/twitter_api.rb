16b382073dddff510bf3f30bfa88e3afaa55b887
Andrew Baik <and.baik@gmail.com>
2016-05-20 05:08:37 -0700

    fixed some typos

--- app/api/twitter_api.rb
+++ app/api/twitter_api.rb
@@ -3,11 +3,11 @@ class TwitterAPI
 
   def initialize
     @twitter_client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_key        = Rails.application.consumer_key
       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
       config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
       config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
     end
   end
-  
+
 end
--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,10 +1,10 @@
 class StatusesController < ApplicationController
   def index
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_SECRET"]
     end
 
     @statuses = client.user_timeline("strychemi", count: 5)
